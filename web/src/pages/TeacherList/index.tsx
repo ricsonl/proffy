10833ab8e7ee9ca9e2d1d21b874df36407630caa
ricsonl <ricsonluizv@gmail.com>
2020-08-11 10:15:42 -0300

    study page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
 
 import styles from './TeacherList.module.css';
 
 const TeacherList = () => {
-    return (
-        <div className={styles.container}>
-            
+  return (
+    <div className={`${styles.page}`}>
+      <header>
+        <div className={styles.topBarContainer}>
+          <Link to="/">
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
         </div>
-    );
+
+        <div className={styles.headerContent}>
+          <strong>Estes são os proffys disponíveis.</strong>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default TeacherList;
