637f622090476ae3c22147c8c7cd2a679e25c266
ricsonl <ricsonluizv@gmail.com>
2020-08-11 22:10:47 -0300

    styling

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Textarea from '../../components/TextArea';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -19,10 +22,27 @@ const TeacherForm = () => {
             <Input name="name" label="Nome completo" />
             <Input name="avatar" label="Avatar" />
             <Input name="whatsapp" label="Whatsapp" />
+            <Textarea name="bio" label="Biografia"/>
           </fieldset>
           <fieldset>
             <legend>Sobre a aula</legend>
-            <Input name="subject" label="Matéria" />
+            <Select
+              name="subject"
+              label="Matéria"
+              options={[
+                { value: "Artes", label: "Artes" },
+                { value: "Biologia", label: "Biologia" },
+                { value: "Química", label: "Química" },
+                { value: "Física", label: "Física" },
+                { value: "Matemática", label: "Matemática" },
+                { value: "Geografia", label: "Geografia" },
+                { value: "História", label: "História" },
+                { value: "Filosofia", label: "Filosofia" },
+                { value: "Sociologia", label: "Sociologia" },
+                { value: "Portugês", label: "Portugês" },
+                { value: "Literatura", label: "Literatura" },
+              ]}
+            />
             <Input name="cost" label="Custo da sua hora por aula" />
           </fieldset>
           <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles.module.css';
@@ -11,8 +12,36 @@ const TeacherList = () => {
     <div className={styles.page}>
       <PageHeader title="Estes são os proffys disponíveis.">
         <form>
-          <Input name="subject" label="Matéria"/>
-          <Input name="week_day" label="Dia da semana"/>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Química", label: "Química" },
+              { value: "Física", label: "Física" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Filosofia", label: "Filosofia" },
+              { value: "Sociologia", label: "Sociologia" },
+              { value: "Portugês", label: "Portugês" },
+              { value: "Literatura", label: "Literatura" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
           <Input name="time" label="Hora"/>
         </form>
       </PageHeader>
