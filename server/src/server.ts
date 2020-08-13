cb26f24740c09a6dde86a94e8f4652381e2bcb08
ricsonl <ricsonluizv@gmail.com>
2020-08-13 00:23:53 -0300

    server origin url

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: `http://localhost:3000`,
+  optionsSuccessStatus: 200
+}));
 app.use(routes);
 
 app.listen(process.env.PORT);
